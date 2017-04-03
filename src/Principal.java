14eda6f282e06170d829e466b5d669b7471c24be
Xoel <xoelveiga@gmail.com>
2017-04-03 19:56:24 +0200

    Method to generate doc 1.0
    not final

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println(generatePassword(true, true, false, true, 13));
+	}
 
 	public static StringBuilder generatePassword(boolean lowercase, boolean uppercase, boolean num, boolean symbol,
 			int length) {
@@ -52,8 +60,33 @@ public class Principal {
 		return password;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(generatePassword(true, true, false, true, 13));
+	public static void generateTxt(String pass, String location, String route,String name) throws IOException{
+		String name1=name;
+		if(name==null){
+			name1="Password";
+		}
+		String route1=route;
+		if (route1==null){
+        route1 ="C:/"+name1+".txt";
+		}else{
+//TODO method to change \ into /
+		}
+        File Txt = new File(route1);
+       
+        if(Txt.exists()) {
+        	try{
+                FileWriter fw=new FileWriter(route1,true);
+                fw.write("		"+location+" --> "+pass+"		");
+                fw.close();
+            }catch(IOException e){
+                System.out.println("Error E/S: "+e);
+            }
+        } else {
+        	 BufferedWriter bw;
+            bw = new BufferedWriter(new FileWriter(Txt));
+            bw.write(location+" --> "+pass+"		");
+            bw.close();
+        }
+       
 	}
-
 }
